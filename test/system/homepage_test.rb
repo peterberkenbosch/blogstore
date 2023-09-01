e430f23dfec2939f6cc5580698951243f9af1436
Peter Berkenbosch <info@peterberkenbosch.nl>
2023-09-01 19:57:38 +0200

    Show the store name on the homepage

--- test/system/homepage_test.rb
+++ test/system/homepage_test.rb
@@ -1,8 +1,12 @@
 require "application_system_test_case"
 
 class HomepageTest < ApplicationSystemTestCase
+  setup do
+    @store = create(:store, name: "BlogStore")
+  end
+
   test "homepage" do
     visit root_url
-    assert_selector "h1", text: "Home#index"
+    assert_selector "h1", text: "BlogStore"
   end
 end
